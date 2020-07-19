0aced9c743ea2410324239404382c1223e481da4
Chinmay <49972966+ckjoshi1997@users.noreply.github.com>
2020-07-19 14:02:17 -0700

    starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Chinmay from "./profile.jpg";
+import "./App.css";
+
+function Header({ title }) {
+  return (
+    <div
+      style={{
+        backgroundColor: "#e3fcfc",
+        color: "black",
+        height: "50px",
+        fontSize: "30px",
+        textAlign: "center",
+        margin: "auto",
+        width: "50%",
+        border: "10px solid #42f5f2",
+        padding: "20px",
+      }}
+    >
+      {title}
+    </div>
+  );
+}
+
+function Photo({ picture }) {
+  return (
+    <img
+      style={{
+        width: "40%",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        borderRadius: "80%",
+      }}
+      src={picture}
+    />
+  );
+}
+
+function Card({ title, body, color }) {
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      {title}
+      <br />
+      {body}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="Chinmay's first website" />
+      <Photo picture={Chinmay} />
+    </>
   );
 }
 
